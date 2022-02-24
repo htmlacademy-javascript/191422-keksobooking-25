7219586f42079f908d210ce5bb023d2b0f5af81e
Ivan Zharkov <stalyer@yandex.ru>
2022-02-24 19:23:54 +0700

    Доработал функции получения рандомных чисел

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
-const getRandomNumberInteger = (minNum, maxNum) => {
+const getRandomNumber = (minNum, maxNum) => {
   if (minNum < 0 || maxNum < 0) {
     throw new Error('Числа не должны быть отрицательными');
   }
   if (minNum > maxNum) {
     throw new Error('Диапазон "от" и "до" задан неверно');
   }
+  return minNum + Math.random() * (maxNum - minNum + 1);
+};
+
+const getRandomNumberInteger = (minNum, maxNum) => {
   minNum = Math.ceil(minNum);
   maxNum = Math.floor(maxNum);
-  return Math.floor(minNum + Math.random() * (maxNum - minNum + 1));
+  return Math.floor(getRandomNumber(minNum, maxNum));
 };
 
 getRandomNumberInteger(1, 10);
 
-const getRandomNumberFloat = (minNum, maxNum, decimal) => {
-  if (minNum < 0 || maxNum < 0) {
-    throw new Error('Числа не должны быть отрицательными');
-  }
-  if (minNum > maxNum) {
-    throw new Error('Диапазон "от" и "до" задан неверно');
-  }
-  return parseFloat((minNum + Math.random() * (maxNum - minNum + 1)).toFixed(decimal));
-};
+const getRandomNumberFloat = (minNum, maxNum, decimal = 1) => parseFloat(getRandomNumber(minNum, maxNum).toFixed(decimal));
 
 getRandomNumberFloat(1, 10, 5);
