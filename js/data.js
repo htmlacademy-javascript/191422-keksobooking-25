dd39b5f7341b5e40b7da0227ae06b6a663aef467
Ivan Zharkov <stalyer@yandex.ru>
2022-03-17 18:36:33 +0700

    Убрал лишние префиксы у констант

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 import {getRandomNumberInteger, getRandomNumberFloat, getRandomArrayElement} from './util.js';
 
-const AD_TYPE = [
+const TYPE = [
   'palace',
   'flat',
   'house',
@@ -8,13 +8,13 @@ const AD_TYPE = [
   'hotel'
 ];
 
-const AD_TIME = [
+const TIME = [
   '12:00',
   '13:00',
   '14:00'
 ];
 
-const AD_FEATURES = [
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -23,18 +23,18 @@ const AD_FEATURES = [
   'conditioner'
 ];
 
-const AD_PHOTOS = [
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
-const AD_LOCATION_LAT = {
+const LOCATION_LAT = {
   min: 35.65000,
   max: 35.70000
 };
 
-const AD_LOCATION_LNG = {
+const LOCATION_LNG = {
   min: 139.70000,
   max: 139.80000
 };
@@ -43,8 +43,8 @@ const createCardAd = (index) => {
   index = index + 1 < 10 ? `0${index + 1}` : index + 1;
 
   const location = {
-    lat: getRandomNumberFloat(AD_LOCATION_LAT.min, AD_LOCATION_LAT.max, 5),
-    lng: getRandomNumberFloat(AD_LOCATION_LNG.min, AD_LOCATION_LNG.max, 5)
+    lat: getRandomNumberFloat(LOCATION_LAT.min, LOCATION_LAT.max, 5),
+    lng: getRandomNumberFloat(LOCATION_LNG.min, LOCATION_LNG.max, 5)
   };
 
   return {
@@ -55,14 +55,14 @@ const createCardAd = (index) => {
       title: 'Уютное гнездышко для молодоженов',
       address: `${location.lat}, ${location.lng}`,
       price: getRandomNumberInteger(500, 100000),
-      type: getRandomArrayElement(AD_TYPE),
+      type: getRandomArrayElement(TYPE),
       rooms: getRandomNumberInteger(1, 5),
       guests: getRandomNumberInteger(1, 10),
-      checkin: getRandomArrayElement(AD_TIME),
-      checkout: getRandomArrayElement(AD_TIME),
-      features: AD_FEATURES.slice(0, getRandomNumberInteger(1, AD_FEATURES.length - 1)),
+      checkin: getRandomArrayElement(TIME),
+      checkout: getRandomArrayElement(TIME),
+      features: FEATURES.slice(0, getRandomNumberInteger(1, FEATURES.length - 1)),
       description: 'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.',
-      photos: AD_PHOTOS.slice(0, getRandomNumberInteger(1, AD_PHOTOS.length - 1))
+      photos: PHOTOS.slice(0, getRandomNumberInteger(1, PHOTOS.length - 1))
     },
     location
   };
