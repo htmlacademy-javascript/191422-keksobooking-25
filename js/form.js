55324540268968c40e74076398b38357db84e040
Ivan Zharkov <stalyer@yandex.ru>
2022-04-02 12:49:29 +0700

    Добавил валидацию цены и времени

--- js/form.js
+++ js/form.js
@@ -5,12 +5,23 @@ const fieldTitle = form.querySelector('#title');
 const fieldPrice = form.querySelector('#price');
 const fieldRoom = form.querySelector('#room_number');
 const fieldCapacity = form.querySelector('#capacity');
+const fieldType = form.querySelector('#type');
+const fieldTimeIn = form.querySelector('#timein');
+const fieldTimeOut = form.querySelector('#timeout');
 
 const QUANTITY_CAPACITY = {
   1: [1],
   2: [1, 2],
   3: [1, 2, 3],
   100: [0]
+};
+
+const OFFER_TYPES_PRICE_MIN = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000
 };
 
 const pristine = new Pristine(form, {
@@ -25,8 +36,10 @@ const validator = {
     return value.length >= 30 && value.length <= 100;
   },
 
-  isPriceValid(value) {
-    return value >= 0 && value <= 100000;
+  isPriceValid() {
+    const priceValue = parseInt(fieldPrice.value, 10);
+    const offerTypeValue = fieldType.value;
+    return priceValue >= OFFER_TYPES_PRICE_MIN[offerTypeValue] && priceValue <= 100000;
   },
 
   isQuantityCapacityValid() {
@@ -59,13 +72,34 @@ const errorMessage = {
       return 'Кол-во комнат должно быть 100';
     }
     return 'Кол-во гостей должно быть меньше';
+  },
+
+  getPriceText() {
+    const offerTypeValue = fieldType.value;
+    const formatter = new Intl.NumberFormat('ru');
+    return `Цена должна быть от ${formatter.format(OFFER_TYPES_PRICE_MIN[offerTypeValue])} до 100 000`;
   }
 };
 
 pristine.addValidator(fieldTitle, validator.isTitileLengthValid, 'Длинна заголовка должна быть от 30 до 100 символов', 2, true);
-pristine.addValidator(fieldPrice, validator.isPriceValid, 'Цена должна быть от 0 до 100 000', 2, true);
+pristine.addValidator(fieldPrice, validator.isPriceValid, errorMessage.getPriceText, 2, true);
 pristine.addValidator(fieldRoom, validator.isQuantityCapacityValid, errorMessage.getRoomText);
 pristine.addValidator(fieldCapacity, validator.isQuantityCapacityValid, errorMessage.getCapacityText);
+
+fieldType.addEventListener('change', (evt) => {
+  pristine.validate(fieldPrice);
+  const priceMinValue = OFFER_TYPES_PRICE_MIN[evt.target.value];
+  fieldPrice.min = priceMinValue;
+  fieldPrice.placeholder = priceMinValue;
+});
+
+fieldTimeIn.addEventListener('change', (evt) => {
+  fieldTimeOut.value = evt.target.value;
+});
+
+fieldTimeOut.addEventListener('change', (evt) => {
+  fieldTimeIn.value = evt.target.value;
+});
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
