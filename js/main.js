3f7b9adee8a7bf3180acccaa0ff323fb8b5bb62b
Ivan Zharkov <stalyer@yandex.ru>
2022-04-04 18:37:14 +0700

    Доработал по замечаниям

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 import {page} from './page.js';
-import {createMap} from'./map.js';
 import './form.js';
+import {createOffers} from './data.js';
+import {Map} from './map.js';
+
+const init = () => {
+  const offers = createOffers();
+  new Map('map-canvas', offers);
+};
 
 page.setInactive();
-createMap();
+
+if (document.readyState === 'interactive' || document.readyState === 'complete') {
+  init();
+} else {
+  document.addEventListener('DOMContentLoaded', init);
+}
--- js/map.js
+++ js/map.js
@@ -1,77 +1,81 @@
 import {page} from './page.js';
-import {createOffers} from './data.js';
 import {cardsGenerator} from './card.js';
 
 const fieldAddress = document.querySelector('#address');
-const offers = createOffers();
 
 const CENTER_MAP = {
   lat: 35.66322,
   lng: 139.77938
 };
 
-const createMap = () => {
-  const map = L.map('map-canvas')
-    .on('load', () => {
+class Map {
+  constructor(canvasSelector, offers) {
+    this._map = this._createMap(canvasSelector);
+    this._setTileLayers();
+    this._addMainMarker();
+    this._addOfferMarkers(offers);
+  }
+
+  _createMap(canvasSelector) {
+    const onLoadMap = () => {
       page.setActive();
       fieldAddress.value = `${CENTER_MAP.lat}, ${CENTER_MAP.lng}`;
-    })
-    .setView(CENTER_MAP, 12);
+    };
+    return L.map(canvasSelector).on('load', onLoadMap).setView(CENTER_MAP, 12);
+  }
 
-  L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
-    }
-  ).addTo(map);
+  _setTileLayers() {
+    const layersURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const options = {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    };
+    L.tileLayer(layersURL, options).addTo(this._map);
+  }
 
-  const mainPinIcon = L.icon({
-    iconUrl: './img/main-pin.svg',
-    iconSize: [52, 52],
-    iconAnchor: [26, 52]
-  });
+  _addMainMarker() {
+    const mainPinIcon = L.icon({
+      iconUrl: './img/main-pin.svg',
+      iconSize: [52, 52],
+      iconAnchor: [26, 52]
+    });
 
-  const mainPinMarker = L.marker(
-    CENTER_MAP,
-    {
-      draggable: true,
-      icon: mainPinIcon
-    },
-  );
-
-  const markerGroup = L.layerGroup().addTo(map);
-
-  const offerPinIcon = L.icon({
-    iconUrl: './img/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40]
-  });
-
-  const createMarker = (offer) => {
-    const {lat, lng} = offer.location;
-    const marker = L.marker(
-      {
-        lat,
-        lng,
-      },
-      {
-        offerPinIcon,
+    const mainPinMarker = L.marker(
+      CENTER_MAP, {
+        draggable: true,
+        icon: mainPinIcon
       },
     );
 
-    marker.addTo(markerGroup).bindPopup(cardsGenerator.createSingleCard(offer));
-  };
+    mainPinMarker.addTo(this._map);
 
-  mainPinMarker.addTo(map);
+    mainPinMarker.on('moveend', (evt) => {
+      const location = evt.target.getLatLng();
+      fieldAddress.value = `${location.lat.toFixed(5)}, ${location.lng.toFixed(5)}`;
+    });
+  }
 
-  mainPinMarker.on('moveend', (evt) => {
-    const location = evt.target.getLatLng();
-    fieldAddress.value = `${location.lat.toFixed(5)}, ${location.lng.toFixed(5)}`;
-  });
+  _addOfferMarkers(offers) {
+    const markerGroup = L.layerGroup().addTo(this._map);
 
-  offers.forEach((offer) => {
-    createMarker(offer);
-  });
-};
+    const offerPinIcon = L.icon({
+      iconUrl: './img/pin.svg',
+      iconSize: [40, 40],
+      iconAnchor: [20, 40]
+    });
 
-export {createMap};
+    const createMarker = (offer) => {
+      const marker = L.marker(
+        offer.location,
+        {offerPinIcon},
+      );
+
+      marker.addTo(markerGroup).bindPopup(cardsGenerator.createSingleCard(offer));
+    };
+
+    offers.forEach((offer) => {
+      createMarker(offer);
+    });
+  }
+}
+
+export {Map};
